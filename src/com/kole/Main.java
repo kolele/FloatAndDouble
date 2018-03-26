1e351e5b726382246ce7c7ff6e1c8e114cf3d444
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-26 18:23:35 +0200

    a

---
+++ src/com/kole/Main.java
@@ -0,0 +1,22 @@
+package com.kole;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //width of int i= 32 (4 bytes).
+        int myIntValue = 5 / 2;
+        //width of float = 32 (4 bytes)
+        float myFloatValue = 5f / 3f;
+        //width of double = 64 (8 bytes)
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleVlaue = " + myDoubleValue);
+
+        int numberOfPounds = 5;
+        double kilograms = numberOfPounds * 0.45359237;
+        System.out.println(numberOfPounds + " pounds equals " + kilograms + " kilograms");
+    }
+}
